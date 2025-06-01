8631443e8ed24b8ca858d3e87ecee4ec6f7cb9f0
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-06-01 18:37:46 +0600

    bgn pro 7 testimonials

--- bgnProjects/src/App.jsx
+++ bgnProjects/src/App.jsx
@@ -13,8 +13,8 @@ import Nine from './p9/Nine';
 const App = () => {
   return (
     <div id="home">
-      {/* <Six />
-      <hr /> */}
+      <Seven />
+      <hr />
       <Hero />
       <hr />
       <One />
@@ -29,8 +29,8 @@ const App = () => {
       <hr />
       <Six />
       <hr />
-      <Seven />
-      <hr />
+      {/* <Seven />
+      <hr /> */}
       <Eight />
       <hr />
       <Nine />
@@ -55,7 +55,7 @@ const Hero = () => {
           <a href="#four">Project four: Calculator</a>
           <a href="#five">Project five: Theme Toggler</a>
           <a href="#six">Project six: Search Bar Show Hide</a>
-          <a href="#seven">Project seven:</a>
+          <a href="#seven">Project seven: Testimonial</a>
           <a href="#eight">Project eight:</a>
           <a href="#nine">Project nine:</a>
           <a href="#ten">Project ten:</a>
--- bgnProjects/src/p7/Seven.jsx
+++ bgnProjects/src/p7/Seven.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import { useState } from 'react';
+import './s7.css';
 
 const Seven = () => {
+  const [activeItem, setActiveItem] = useState(0);
+  const testimonials = [
+    {
+      testimonial:
+        'Used it once, now my Discord blew up and Elon asked me for tech support.',
+      user: 'Beluga',
+    },
+    {
+      testimonial:
+        'Much wow. Very product. Now I ride rocket to moon with pockets full of treats.',
+      user: 'Doge',
+    },
+    {
+      testimonial:
+        'Clicked once, unlocked brain’s third hemisphere. Now I float and give wisdom.',
+      user: 'Xavier',
+    },
+  ];
+
+  const handlePrev = () => {
+    if (activeItem === 0) setActiveItem(testimonials.length - 1);
+    else setActiveItem(activeItem - 1);
+  };
+  const handleNext = () => {
+    setActiveItem((activeItem + 1) % testimonials.length);
+  };
+
   return (
-   <div id="seven">
+    <div id="seven">
       <section>
-        <h1>Seven</h1>
+        <h1>7. Testimonials</h1>
+        <div className="container">
+          <div className="testimonial">
+            <h3>{testimonials[activeItem].testimonial}</h3>
+            <p>{testimonials[activeItem].user}</p>
+            <div className="btns">
+              <button onClick={handlePrev}>Prev</button>
+              <button onClick={handleNext}>Next</button>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Seven
+export default Seven;
